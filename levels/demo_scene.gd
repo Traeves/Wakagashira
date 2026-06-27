5673933e577e82669d39038c1855c6717bedc899
Travis Harrison <146888549+Traeves@users.noreply.github.com>
2026-06-26 21:37:09 -0400

    cleaned up demo scene

--- levels/demo_scene.gd
+++ levels/demo_scene.gd
@@ -1,16 +1,14 @@
 extends Node
 
-@onready var start_position := $Marker2D
 @onready var player := $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#player.x = get_viewport().get_visible_rect().size.x/2
-	#player.y = get_viewport().get_visible_rect().size.y/2
-	pass
 	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
